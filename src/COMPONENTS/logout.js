1845e93c436e316fcb3079c7927666f4867da809
Marcus Kitchen <marcusdkitchen@gmail.com>
2024-02-14 14:06:37 -0500

    logout

--- src/COMPONENTS/logout.js
+++ src/COMPONENTS/logout.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { redirect } from "react-router-dom";
 
 function Logout(){
+    const [loggedOut, setLoggedOut] = useState()
 
+    useEffect(() => {
+        fetch('http://localhost:3000/users', {
+            method: 'DELETE'
+        })
+        .then(res => {
+            if(res.ok) {
+                setLoggedOut(true)
+            } else {
+                setLoggedOut(false)
+            }
+        })
+
+    }, [])
+
+    return (
+        <p>{loggedOut ? "Logged Out" : 'Error!!'}</p>
+    )
 }
 export default Logout;
