13e4e44c0f363d3d3689e8d5f8b72a161a44bb67
Marcus Kitchen <marcusdkitchen@gmail.com>
2024-02-15 10:36:55 -0500

    mk branch up to date

--- src/COMPONENTS/home.js
+++ src/COMPONENTS/home.js
@@ -7,7 +7,7 @@ function Home() {
             <p className="intro">
                 Welcome to the <b>RECIPE CORNER!</b> This website is a digital cookbook for you to keep all your recipes in one convenient place!!
                 <br />
-                Feel free to explore the <a href="http://localhost:3001/recipes">cookbook</a> or sign up to add a new recipe on the <a href="http://localhost:3001/form">FORM</a> page!  
+                Feel free to explore the <a href="http://localhost:3000/recipes">cookbook</a> or sign up to add a new recipe on the <a href="http://localhost:3000/form">FORM</a> page!  
             </p>
         </div>
     )
